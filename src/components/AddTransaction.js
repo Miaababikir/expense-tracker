e5efe62b740ba49b386417ff2079141eab9d3539
Mosab Ibrahim <miaababikir@gmail.com>
2020-07-08 13:36:22 +0200

    Wrapping up

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, {useContext, useState} from 'react';
+import {GlobalContext} from "../context/GlobalState";
 
 const AddTransaction = () => {
+
+    const {addTransaction} = useContext(GlobalContext);
 
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
 
+    function handelSubmit(e) {
+        e.preventDefault();
+
+        addTransaction({
+            id: Math.random() * 100000000,
+            text,
+            amount: parseInt(amount)
+        });
+
+        setText('');
+        setAmount(0);
+    }
+
     return (
         <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={handelSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={e => setText(e.target.value)} placeholder="Enter text..."/>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React, {createContext, useReducer} from 'react';
 import AppReducer from "./AppReducer";
 
 const initialState = {
-    transactions: [
-        {id: 1, text: 'Flower', amount: -20},
-        {id: 2, text: 'Salary', amount: 300},
-        {id: 3, text: 'Book', amount: -10},
-        {id: 4, text: 'Camera', amount: 150}
-    ]
+    transactions: []
 };
 
 export const GlobalContext = React.createContext(initialState);
@@ -15,7 +10,27 @@ export const GlobalContext = React.createContext(initialState);
 export const GlobalProvider = ({children}) => {
     const [state, distpatch] = useReducer(AppReducer, initialState);
 
-    return (<GlobalContext.Provider value={ {transactions: state.transactions }}>
+    // Actions
+    function deleteTransaction(id) {
+        distpatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+    function addTransaction(transaction) {
+        distpatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
         {children}
     </GlobalContext.Provider>)
 }
